a9c8db64aed1debda33c3d04b46c9be2f282848e
adrian-caltea <adicaltea@gmail.com>
2022-07-23 15:59:20 +0300

    nav bar style and user drop-down

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,24 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
+import { User } from '../models/user';
+import { AccountService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  content?: string;
+  user: User;
 
-  constructor(private userService: UserService) { }
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.userService.getPublicContent().subscribe(
-      (data: string | undefined) => {
-        this.content = data;
-      },
-      (err: { error: string; }) => {
-        // this.content = JSON.parse(err.error).message;
-      }
-    );
+    this.user = this.accountService.userValue;
   }
 }
